03f18faf11814de61c6ba0866d7388b179ced2f4
Reece Crook <reececrook16@gmail.com>
2023-12-02 16:00:25 -0700

    added contact feature

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser';
+import "../Contact.css"
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_pda4dyn', 'template_4npi60d', form.current, 'JiyFMUBN4XcgzPCpE')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset()
+  };
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className='container'>
+      <div className="text">
+        Contact us Form
+      </div>
+      <form ref={form} onSubmit={sendEmail}>
+        <div className='formRow'>
+          <div className='inputData'>
+            <label htmlFor="userFirstName">First Name:</label>
+            <input type='text' id='userFirstName' name='userFirstName' required></input><br />
+          </div>
+          <div className='inputData'>
+            <label htmlFor="userLastName">Last Name:</label>
+            <input type='text' id='userLastName' name='userLastName' required></input><br />
+          </div>
+        </div>
+        <div className='formRow'>
+          <div className='inputData'>
+            <label htmlFor="userEmail">Email:</label>
+            <input type='text' id='userEmail' name='userEmail' required></input><br />
+          </div>
+          <div className='inputData'>
+            <label htmlFor="userPhoneNum">Phone#:</label>
+            <input type='text' id='userPhoneNum' name='userPhoneNum' required></input><br />
+          </div>
+        </div>
+        <div className="form-row">
+         <div className="input-data textarea">
+            <label htmlFor="userMessage">Write your message</label><br />
+            <textarea id='userMessage' rows="8" cols="80" name="userMessage" required></textarea><br />
+            <input className='submitBtn' type="submit"></input>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
