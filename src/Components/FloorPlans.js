a1c404417be2b347ab456ff90330e4ce901813ee
ReeceCrook <reececrook16@gmail.com>
2024-01-13 11:14:32 -0700

    Adjusted floorPlan styling and contact position

--- src/Components/FloorPlans.js
+++ src/Components/FloorPlans.js
@@ -9,29 +9,37 @@ function FloorPlans() {
   return (
     <div className='floorPlanWrapper'>
       <div className='card1'>
-        <ImageCarousel pics={floorPlanImages[0]} />
-        <h2 style={{"fontWeight": "bold"}}>3 Bedroom 2.5 Bath</h2>
-        <p>
-          Unfinished Basement<br />
-          1 car detached garage
-        </p>
-      </div>
-      <div className='card2'>
-        <ImageCarousel pics={floorPlanImages[1]} />
-          <h2 style={{"fontWeight": "bold"}}>2 Bedroom 2.5 Bath</h2>
+        <div className='cardTextDiv'>
+          <h2>3 Bedroom 2.5 Bath</h2>
           <p>
             Unfinished Basement<br />
             1 car detached garage
           </p>
+        </div>
+        <ImageCarousel pics={floorPlanImages[0]} />
+        
+      </div>
+      <div className='card2'>
+        <div className='card2TextDiv'>
+          <h2>2 Bedroom 2.5 Bath</h2>
+          <p>
+            Unfinished Basement<br />
+            1 car detached garage
+          </p>
+        </div>
+        <ImageCarousel pics={floorPlanImages[1]} />
       </div>
       <div className='card3'>
+        <div className='card3TextDiv'>
+          <h2>3 Bedroom 2.5 Bath</h2>
+          <p>
+            Master on the Main<br />
+            Unfinished Basement<br />
+            2 car detached garage
+          </p>
+        </div>
+        
         <ImageCarousel pics={floorPlanImages[2]} />
-        <h2 style={{"fontWeight": "bold"}}>3 Bedroom 2.5 Bath</h2>
-        <p>
-          Master on the Main<br />
-          Unfinished Basement<br />
-          2 car detached garage
-        </p>
       </div>
     </div>
   )
