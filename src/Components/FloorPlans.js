2149de75f4a06317f1579ce5e0fdd8ad2769324f
ReeceCrook <reececrook16@gmail.com>
2024-01-13 02:01:10 -0700

    Changed nav Drop down location, added more floorplans and fixed size changing, and scooted contact over a bit

--- src/Components/FloorPlans.js
+++ src/Components/FloorPlans.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import '../App.css'
+import '../FloorPlan.css'
  import { floorPlanImages } from '../carousel'
  import ImageCarousel from './ImageCarousel'
+ 
 
 
 function FloorPlans() {
@@ -9,17 +10,28 @@ function FloorPlans() {
     <div className='floorPlanWrapper'>
       <div className='card1'>
         <ImageCarousel pics={floorPlanImages[0]} />
-        <h2 style={{"font-weight": "bold"}}>3 Bedroom 2.5 Bath</h2>
+        <h2 style={{"fontWeight": "bold"}}>3 Bedroom 2.5 Bath</h2>
         <p>
           Unfinished Basement<br />
           1 car detached garage
         </p>
       </div>
       <div className='card2'>
-        
+        <ImageCarousel pics={floorPlanImages[1]} />
+          <h2 style={{"fontWeight": "bold"}}>2 Bedroom 2.5 Bath</h2>
+          <p>
+            Unfinished Basement<br />
+            1 car detached garage
+          </p>
       </div>
       <div className='card3'>
-        
+        <ImageCarousel pics={floorPlanImages[2]} />
+        <h2 style={{"fontWeight": "bold"}}>3 Bedroom 2.5 Bath</h2>
+        <p>
+          Master on the Main<br />
+          Unfinished Basement<br />
+          2 car detached garage
+        </p>
       </div>
     </div>
   )
