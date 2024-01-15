a311dca64c0e5a603e772c34c47f1546dc2f07e6
ReeceCrook <reececrook16@gmail.com>
2024-01-15 14:50:40 -0700

    Another attempt at fixing mobile safari gallery fullscreen.

--- src/Components/ImageCarousel.js
+++ src/Components/ImageCarousel.js
@@ -1,22 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ImageGallery from "react-image-gallery";
 
 function ImageCarousel({ pics, showFullscreenButton=true, onClick=null  }){
-    // const [isFullscreen, setIsFullscreen] = useState(false);
+    const [isFullscreen, setIsFullscreen] = useState(false);
 
-    // const toggleFullscreen = () => {
-    //     const container = document.querySelector('.image-gallery-fullscreen-button');
-    
-    //     if (container) {
-    //       if (isFullscreen) {
-    //         document.exitFullscreen();
-    //       } else {
-    //         container.click();
-    //       }
-    
-    //       setIsFullscreen((prevFullscreen) => !prevFullscreen);
-    //     }
-    //   };
+    function fullscreenHandler() {
+        const homeTabs = document.querySelector('.tabs.homeTabs')
+        const navTabs = document.querySelector('.tabs.dropdown')
+        const container = document.querySelector('.image-gallery-fullscreen-button');
+
+        if(container){
+            console.log(container)
+
+            container.addEventListener('click', () => {
+                setIsFullscreen((currnet) => !currnet)
+            })
+        }
+
+        if (isFullscreen === true) {
+            if (homeTabs) homeTabs.style.display = 'none';
+            if (navTabs) navTabs.style.display = 'none';
+        }
+
+        if(isFullscreen === true) {
+            homeTabs.style.display = 'none';
+            navTabs.style.display = 'none';
+        }
+    }
+
+    useEffect(() => {
+        fullscreenHandler();
+    }, []);
 
     return (
         <div className='carousel-wrapper'>
