f8b0c3e6643f3348f366deac89beaa2e9974e21b
ReeceCrook <reececrook16@gmail.com>
2024-01-15 13:55:50 -0700

    Changed community info font size and working on fixing gallery fullscreen for safari.

--- src/Components/ImageCarousel.js
+++ src/Components/ImageCarousel.js
@@ -1,16 +1,28 @@
-import React from "react";
-import { homeImages } from "../carousel";
+import React, { useState } from "react";
 import ImageGallery from "react-image-gallery";
 
-function ImageCarousel({ pics, fullscreenBooleen=false, showFullscreenButton=true, onClick=null  }){
+function ImageCarousel({ pics, showFullscreenButton=true, onClick=null  }){
+    // const [isFullscreen, setIsFullscreen] = useState(false);
+
+    // const toggleFullscreen = () => {
+    //     const container = document.querySelector('.image-gallery-fullscreen-button');
+    
+    //     if (container) {
+    //       if (isFullscreen) {
+    //         document.exitFullscreen();
+    //       } else {
+    //         container.click();
+    //       }
+    
+    //       setIsFullscreen((prevFullscreen) => !prevFullscreen);
+    //     }
+    //   };
 
     return (
         <div className='carousel-wrapper'>
             <ImageGallery 
                 items={pics} 
                 showFullscreenButton={showFullscreenButton} 
-                onClick={onClick} 
-                isFullscreen={true} 
             />
       </div>
     )
