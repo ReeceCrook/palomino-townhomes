cb59ba99a7ed7e6a6e3bd1a2acff4a276baaff53
ReeceCrook <reececrook16@gmail.com>
2024-01-15 16:51:46 -0700

    Still trying to fix fullscreen. Hopefully this is the one.

--- src/Components/ImageCarousel.js
+++ src/Components/ImageCarousel.js
@@ -3,35 +3,30 @@ import ImageGallery from "react-image-gallery";
 
 function ImageCarousel({ pics, showFullscreenButton=true, onClick=null  }){
     const [isFullscreen, setIsFullscreen] = useState(false);
-    
 
-    function fullscreenHandler() {
-        const homeTabs = document.querySelector('.homeTabs');
-        const navTabs = document.querySelector('.dropdown');
-        const container = document.querySelector('.image-gallery-fullscreen-button');
+    const homeTabs = document.querySelector('.homeTabs');
+    const navTabs = document.querySelector('.dropdown');
+    const container = document.querySelector('.image-gallery-fullscreen-button');
 
-        if(container){
-            container.addEventListener('click', () => {
-                setIsFullscreen((current) => !current);
-            })
-        }
-
-        const viewportWidth = window.innerWidth;
-
-        if (isFullscreen === true) {
-            if(homeTabs) homeTabs.classList.add('hidden');
-            if(navTabs) navTabs.classList.add('hidden');
-        } else {
-            if(homeTabs) {
-                homeTabs.classList.remove('hidden')
-            }
-            if(navTabs) navTabs.classList.remove('hidden');
-        }
+    if(container){
+        container.addEventListener('click', () => {
+            setIsFullscreen((current) => !current);
+        })
     }
 
-    useEffect(() => {
-        fullscreenHandler();
-    }, [isFullscreen]);
+    if (isFullscreen === true) {
+        if(homeTabs) homeTabs.classList.add('hidden');
+        if(navTabs) navTabs.classList.add('hidden');
+
+    } else {
+        if(homeTabs) {
+            homeTabs.classList.remove('hidden')
+
+        }
+        if(navTabs) navTabs.classList.remove('hidden');
+
+    }
+
 
     return (
         <div className='carousel-wrapper'>
