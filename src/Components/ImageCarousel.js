9bb722064b598c65cef576814d1160b0600c394d
ReeceCrook <reececrook16@gmail.com>
2024-01-15 17:15:12 -0700

    Still trying to fix fullscreen. Hopefully this is the one.

--- src/Components/ImageCarousel.js
+++ src/Components/ImageCarousel.js
@@ -3,30 +3,38 @@ import ImageGallery from "react-image-gallery";
 
 function ImageCarousel({ pics, showFullscreenButton=true, onClick=null  }){
     const [isFullscreen, setIsFullscreen] = useState(false);
+    
 
-    const homeTabs = document.querySelector('.homeTabs');
-    const navTabs = document.querySelector('.dropdown');
-    const container = document.querySelector('.image-gallery-fullscreen-button');
+    const fullscreenHandler = () => {
+        const homeTabs = document.querySelector('.homeTabs');
+        const navTabs = document.querySelector('.dropdown');
+        const container = document.querySelector('.image-gallery-fullscreen-button');
 
-    if(container){
-        container.addEventListener('click', () => {
-            setIsFullscreen((current) => !current);
-        })
-    }
+        if(container){
+            container.addEventListener('click', () => {
+                setIsFullscreen((current) => !current);
+            })
+        }
 
-    if (isFullscreen === true) {
-        if(homeTabs) homeTabs.classList.add('hidden');
-        if(navTabs) navTabs.classList.add('hidden');
+        if (isFullscreen === true) {
+            if(homeTabs) homeTabs.classList.add('hidden');
+            if(navTabs) navTabs.classList.add('hidden');
+            console.log(homeTabs.classList, "1<== homeTabs, navTabs ==>", isFullscreen)
 
-    } else {
-        if(homeTabs) {
-            homeTabs.classList.remove('hidden')
+        } else {
+            if(homeTabs){
+                homeTabs.classList.remove('hidden')
+                console.log(homeTabs.classList, "2<== homeTabs, navTabs ==>", isFullscreen)
+            }
+                if(navTabs && navTabs.classList.contains('hidden')){
+                    navTabs.classList.remove('hidden');
+                } 
 
         }
-        if(navTabs) navTabs.classList.remove('hidden');
-
     }
 
+
+    fullscreenHandler();
 
     return (
         <div className='carousel-wrapper'>
