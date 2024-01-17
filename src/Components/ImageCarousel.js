0dcfe8b0d28e9279cb9735f91a9181a247383902
ReeceCrook <reececrook16@gmail.com>
2024-01-17 12:48:16 -0700

    Added favicons

--- src/Components/ImageCarousel.js
+++ src/Components/ImageCarousel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ImageGallery from "react-image-gallery";
 
 function ImageCarousel({ pics, showFullscreenButton=true, onClick=null  }){
