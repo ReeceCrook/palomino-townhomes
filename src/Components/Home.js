abfb0d868e21bb2190f6a734165cd92e20f39376
ReeceC <reececrook16@gmail.com>
2024-06-26 16:56:33 -0600

    Added buyer incentive photo back into carosel for mobile. Started on open house information bubble, waiting for design instructions.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from '../assets/mainPics/PalominoRanch-Logotype.png'
 import ImageCarousel from './ImageCarousel'
 import { homeImages } from '../images'
@@ -6,16 +6,16 @@ import { homeImages } from '../images'
 
 function Home() {
   const [isMobile, setIsMobile] = useState(false);
-  const [filteredImages, setFilteredImages] = useState([]);
-  const img = homeImages.filter(current => current.id === 4)
+  // const [filteredImages, setFilteredImages] = useState([]);
+  // const img = homeImages.filter(current => current.id === 4)
 
-  const filterImages = useCallback(() => {
-    if (isMobile) {
-      setFilteredImages(homeImages.filter(current => current.id !== 4));
-    } else {
-      setFilteredImages(homeImages);
-    }
-  }, [isMobile]);
+  // const filterImages = useCallback(() => {
+  //   if (isMobile) {
+  //     setFilteredImages(homeImages.filter(current => current.id !== 4));
+  //   } else {
+  //     setFilteredImages(homeImages);
+  //   }
+  // }, [isMobile]);
 
   useEffect(() => {
     const checkIsMobile = () => {
@@ -24,7 +24,7 @@ function Home() {
 
     const handleResize = () => {
       checkIsMobile();
-      filterImages();
+      // filterImages();
     };
 
     checkIsMobile();
@@ -34,24 +34,29 @@ function Home() {
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, [filterImages]);
-
-  useEffect(() => {
-    filterImages();
-  }, [isMobile, filterImages]);
+  }, []);
+  // useEffect(() => {
+  //   filterImages();
+  // }, [isMobile, filterImages]);
 
   return (
     <div className='homeDiv'>
       <img src={Logo} alt='palomino ranch townhomes logo' className='palominoLogo' />
       <div className='homeImageGalleryWrapper'>
-        <ImageCarousel pics={filteredImages} />
+        <ImageCarousel pics={homeImages} />
       </div>
-      <div>
+      <div className={`${isMobile ? 'mobile-circle' : 'circle'}`}>
+          <p style={{ fontSize: "35px", marginTop: "45px"}}>
+            Model Open<br/>
+            Wed-Sun<br/>
+            12p-5p
+          </p>
+        </div>
+      {/* <div>
         {filteredImages === homeImages ? null :
           <img src={img[0].original} alt='First Time Home Buyer Incentive' className='firstTimeIncentive'></img>
         }
-      </div>
-
+      </div> */}
     </div>
   )
 }
