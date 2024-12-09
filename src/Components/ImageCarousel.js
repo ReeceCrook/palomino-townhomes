e9d344e68fe32875dccdd7a689b77ca98e2d40b9
ReeceCrook <reececrook16@gmail.com>
2024-12-09 03:36:34 -0700

    Temperarally removing zoom feature(Need to do some research)

--- src/Components/ImageCarousel.js
+++ src/Components/ImageCarousel.js
@@ -1,6 +1,5 @@
 import React, { useState, useRef } from "react";
 import ImageGallery from "react-image-gallery";
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 function ImageCarousel({ pics, showFullscreenButton = true }) {
     const [isFullScreen, setIsFullScreen] = useState(false);
@@ -33,32 +32,6 @@ function ImageCarousel({ pics, showFullscreenButton = true }) {
                 onScreenChange={handleScreenChange}
                 onClick={handleImageClick}
                 showPlayButton={false}
-                renderItem={(item) => (
-                    <div style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                        <TransformWrapper
-                            initialScale={1}
-                            minScale={1}
-                            maxScale={4}
-                            doubleClick={{ disabled: false }}
-                            pinch={{ step: 5 }}
-                            wrapperStyle={{
-                                width: "100%",
-                                height: "100%",
-                                display: "flex",
-                                justifyContent: "center",
-                                alignItems: "center"
-                            }}
-                        >
-                            <TransformComponent>
-                                <img
-                                    src={item.original}
-                                    alt={item.description || ""}
-                                    style={{ display: "block", margin: "0 auto", maxWidth: "100%", height: "auto" }}
-                                />
-                            </TransformComponent>
-                        </TransformWrapper>
-                    </div>
-                )}
             />
         </div>
     );
